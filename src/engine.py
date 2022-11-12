22aacb4bbb3f5d2c13f907dbba63c76b142f0a25
Jim Pfleger <jim.pfleger@verizon.com>
2022-11-12 15:13:12 -0700

    🚧 Accept ints and operators

--- src/engine.py
+++ src/engine.py
@@ -23,12 +23,44 @@ class Operators(enum.Enum):
 class Engine(Machine):
     def __init__(self):
         Machine.__init__(self, states=States, initial=States.START)
-        self.clear()
+        self.add_transition('input_digit', [States.START, States.ACCUMULATE], States.ACCUMULATE, before=self._input_digit, conditions=self._is_digit)
+        self.add_transition('input_digit', States.COMPUTE, States.ACCUMULATE, before=self._reaccumulate, conditions=self._is_digit)
+        self.add_transition('input_operation', States.ACCUMULATE, States.COMPUTE, before=self._input_operation)
+        self._clear()
 
-    def clear(self):
-        self.pending_op = Operators.NOOP
+    def _clear(self):
+        self.pending_operator = Operators.NOOP
         self.accumulator = 0.0
         self.display = "0"
 
-    def enterDigit(self, digit: int):
-        ...
+    def _is_digit(self, digit: str):
+        return digit in "0123456789"
+
+    def _input_digit(self, digit: str):
+        if self.display == "0":
+            self.display = digit
+        else:
+            self.display += digit
+
+    def _reaccumulate(self, digit: str):
+        self.display = digit
+
+    def _do_pending_operation(self):
+        operand = float(self.display)
+        match self.pending_operator:
+            case Operators.NOOP:
+                return
+            case Operators.ADD:
+                self.accumulator += operand
+            case Operators.SUBTRACT:
+                self.accumulator -= operand
+            case Operators.MULTIPLY:
+                self.accumulator *= operand
+            case Operators.DIVIDE:
+                self.accumulator /= operand
+        self.display = f'{self.accumulator:g}'
+
+    def _input_operation(self, operation: Operators):
+        self._do_pending_operation()
+        self.pending_operator = operation
+        self.accumulator = float(self.display)
