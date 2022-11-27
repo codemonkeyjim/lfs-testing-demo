808f7799e06f87a75832714619e378f2e08999cd
Jim Pfleger <jim.pfleger@verizon.com>
2022-11-27 11:39:10 -0700

    🚧 Basic UI wired to engine

--- features/steps/engine.py
+++ features/steps/engine.py
@@ -1,17 +1,20 @@
-from behave import * # pyright: ignore[reportWildcardImportFromLibrary]
+from behave import *  # pyright: ignore[reportWildcardImportFromLibrary]
 
 from src.engine import Engine, States, Operators
 
-@given('the engine is ready')
+
+@given("the engine is ready")
 def step_impl(context):
     context.engine = Engine()
     assert context.engine.state == States.START
 
-@when('I input {input:d}')
+
+@when("I input {input:d}")
 def step_impl(context, input):
     for digit in str(input):
         context.engine.input_digit(digit)
 
-@then('the display is {expected:d}')
+
+@then("the display is {expected:d}")
 def step_impl(context, expected):
     assert context.engine.display == str(expected)
